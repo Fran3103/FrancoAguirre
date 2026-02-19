3a4eab914311ee72af09c3ba6e4c16d320ebcded
Fran3103 <fn.aguirre@hotmail.com>
2026-02-18 21:22:20 -0300

    Refactor code structure for improved readability and maintainability

--- types.ts
+++ types.ts
@@ -2,19 +2,18 @@ export interface Project {
   id: number;
   title: string;
   description: string;
-  diagram: 'systragos' | 'ticker-api' | 'microservices-ecommerce' | 'bellezaglow';
+  diagram: 'cocktailops' | 'ticketing-api' | 'microservices-ecommerce' | 'bellezaglow';
   tags: string[];
-
   // nuevos
   github?: string;
   demo?: string;
-  docs?: string;   // swagger / postman / readme / etc
-  image?: string[];  // ruta o url de screenshot
+  docs?: string;  
+  image?: string[];  
 }
 
 
 export interface TechItem {
   name: string;
   category: string;
-  icon: string; // URL o ruta al ícono
+  icon: string; 
 }
